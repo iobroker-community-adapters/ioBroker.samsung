e706b9809cde680245fecd81df8e177c93dfffbf
Marian T. <37208979+marian-t-web-de@users.noreply.github.com>
2024-12-02 12:13:10 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-'use strict';
+	'use strict';
 
 const { KEY_VOLDOWN, KEY_MUTE } = require('./keys');
 
@@ -223,7 +223,7 @@ async function main() {
 
 function repeat_main(callback) {
 	try {
-           callback();
+           await callback();
         } catch (err) {
             adapter.log.error(`Connection to TV failed. Is the IP correct? Is the TV switched on?  ${err.message}`)
             adapter.log.error(err.stack);
