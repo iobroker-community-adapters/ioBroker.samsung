e93ed649f9067d8b738831726e9aaf0efde4b3a4
Marian T. <37208979+marian-t-web-de@users.noreply.github.com>
2024-12-05 12:34:43 +0100

    Update main.js

--- main.js
+++ main.js
@@ -292,7 +292,7 @@ function checkPowerOnOff() {
     var cnt = 0, lastOn;
     (function check() {
         isOn(function (on) {
-            adapter.log.debug(`Power on/off check result: ${on} vs lastOn=${lastOn} ({$cnt})`);
+            adapter.log.debug(`Power on/off check result: ${on} vs lastOn=${lastOn}; cnt=${cnt}`);
             if (lastOn !== on) {
                 if (on) {
                     adapter.setState(powerOnOffState, 'ON', true); // uppercase indicates final on state.
